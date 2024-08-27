5bbed74663147a2055ccf9184154437534464ab3
Yashvai14 <vaidyayash36@gmail.com>
2024-08-28 00:44:46 +0530

    first commit

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ document.addEventListener("mousemove", function(dets){
 })
 
 
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale = 3,
+        crsr.style.border = "1px solid #fff",
+        crsr.style.backgroundColor = "transparent"
+    })
+
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1,
+        crsr.style.border = "0px solid #26e1e775",
+        crsr.style.backgroundColor = "#26e1e775"
+    })
+
+})
+
+
 
 gsap.to("#nav",{
     backgroundColor: "#000",
@@ -16,7 +33,7 @@ gsap.to("#nav",{
     scrollTrigger: {
         trigger:"#nav",
         scroller: "body",
-        markers: true,
+       // markers: true,
         start: "top -10%",
         end: "top -11%",
         scrub: 1
@@ -28,9 +45,62 @@ gsap.to("#main",{
     scrollTrigger: {
         trigger:"#main",
         scroller: "body",
-        markers: true,
+       // markers: true,
         start: "top -25%",
         end: "top -70%",
+        scrub: 2
+    }
+})
+
+gsap.from("#about-us img, #about-us-in",{
+    y:50,
+    opacity:0,
+    duration: 2,
+    stagger:0.6,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller: "body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub: 3
+    }
+})
+
+gsap.from(".card",{
+    scale: 0.8,
+    opacity:0,
+    duration: 0.5,
+    scrollTrigger:{
+        trigger:".card",
+        scroller: "body",
+        start:"top 70%",
+        end:"top 65%",
+        scrub: 1
+    }
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+       // markers: true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub: 2
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        //markers: true,
+        start:"top 55%",
+        end:"top 45%",
         scrub: 2
     }
 })
